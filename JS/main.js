b0360f973056155d431de184bc360d38dc5cb745
mandelatuks <manderat206@gmail.com>
2022-04-13 14:49:21 +0200

    Add styling for for home page and about page including sections

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,26 @@
 const humbergerBtn = document.querySelector('.hamburger');
 const closeButton = document.querySelector('.closeb');
 const navMenu = document.querySelector('.nav-menu');
+const navItems = document.querySelectorAll('.nav-link');
+
+function menuToglling() {
+  if (navMenu.classList.contains('menuShow')) {
+    navMenu.classList.remove('menuShow');
+    closeButton.style.dislay = 'none';
+    humbergerBtn.style.dislay = 'block';
+    navMenu.style.dislay = 'none';
+  } else {
+    navMenu.classList.add('menuShow');
+    closeButton.style.dislay = 'block';
+    humbergerBtn.style.dislay = 'none';
+    navMenu.style.dislay = 'block';
+  }
+}
+
+humbergerBtn.addEventListener('click', menuToglling);
+closeButton.addEventListener('click', menuToglling);
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', menuToglling);
+});
+
+
