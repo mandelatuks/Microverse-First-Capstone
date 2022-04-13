439ed681374b81e8fa6bb29f9c52c68e0abf2ebe
mandelatuks <manderat206@gmail.com>
2022-04-13 17:49:14 +0200

    modifie home page and removed the speaker section and rendered it dynamically using js

--- JS/main.js
+++ JS/main.js
@@ -23,4 +23,67 @@ navItems.forEach((navItem) => {
   navItem.addEventListener('click', menuToglling);
 });
 
+const featuredSpeakers = document.querySelector('.speakers-wrapper');
 
+const speakersEvent = [
+  {
+    names: 'Yochai Benkler',
+    image: 'images/image-1.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+  {
+    names: 'Kilnam Chon',
+    image: 'images/image-2.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+  {
+    names: 'SohYeong Noh',
+    image: 'images/image-3.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+  {
+    names: 'Julia Ledar',
+    image: 'images/image-4.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+  {
+    names: 'Lila Tretikov',
+    image: 'images/image-5.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+  {
+    names: 'Ryan Merkey',
+    image: 'images/image-6.png',
+    work: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.',
+    Discription: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet molestiae molestias exercitationem...',
+
+  },
+];
+
+speakersEvent.forEach((guest) => {
+  if (featuredSpeakers) {
+    featuredSpeakers.innerHTML += `<article class="event-speakers">
+    <img src="${guest.image}" alt="speaker image">
+    <div>
+    <h4>${guest.names}</h4>
+    <p class="info-speaker">
+    ${guest.work}
+    </p>
+    <hr class="bottom">
+    <p class="speaker-work"> 
+    ${guest.Discription}
+    </p>
+    </div>
+    </article>`;
+  }
+});
