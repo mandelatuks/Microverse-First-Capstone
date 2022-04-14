c74d9933c6d30df89526d61e599bd829e329225d
mandelatuks <manderat206@gmail.com>
2022-04-14 21:12:58 +0200

    modifie files according to the review requirement

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,7 @@
-// const humbergerBtn = document.querySelector('.hamburger');
-// const closeButton = document.querySelector('.closeb');
-// const navMenu = document.querySelector('.nav-menu');
-// const navItems = document.querySelectorAll('.nav-link');
-
-// function menuToglling() {
-//   if (navMenu.classList.contains('menuShow')) {
-//     navMenu.classList.remove('menuShow');
-//     closeButton.style.dislay = 'none';
-//     humbergerBtn.style.dislay = 'block';
-//     navMenu.style.dislay = 'none';
-//   } else {
-//     navMenu.classList.add('menuShow');
-//     closeButton.style.dislay = 'block';
-//     humbergerBtn.style.dislay = 'none';
-//     navMenu.style.dislay = 'block';
-//   }
-// }
-
-// humbergerBtn.addEventListener('click', menuToglling);
-// closeButton.addEventListener('click', menuToglling);
-// navItems.forEach((navItem) => {
-//   navItem.addEventListener('click', menuToglling);
-// });
-
 const humberg = document.querySelector('.navigation');
 const navList = document.querySelector('.nav-list');
 const times = document.querySelector('.times');
 const navLink = document.querySelectorAll('.nav-link');
-// const modalDesktop = document.querySelector('.modal-container');
-// const modalMobile = document.querySelector('.modal-mobile');
 
 function closeNavItems() {
   navList.classList.replace('open', 'nav-list');
